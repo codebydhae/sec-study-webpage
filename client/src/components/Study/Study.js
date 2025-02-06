15b743eaabfb21f9287630a074516a43f71717b5
“codebydhae” <dhaesha3000@gmail.com>
2025-02-05 23:03:58 -0500

    study guide page added to front end

---
+++ client/src/components/Study/Study.js
@@ -0,0 +1,25 @@
+import './domain.css';
+
+function Study() {
+  return (
+    <div className="main-menu">
+      <button className="main-button" onClick="">
+        <p>Domain 1</p>
+      </button>
+      <button className="main-button" onClick="">
+      <p>Domain 2</p>
+      </button>
+      <button className="main-button" onClick="">
+      <p>Domain 3</p>
+      </button>
+      <button className="main-button" onClick="">
+      <p>Domain 4</p>
+      </button>
+      <button className="main-button" onClick="">
+      <p>Domain 5</p>
+      </button>
+    </div>
+  );
+}
+
+export default Study;
---
+++ client/src/pages/StudyGuide/StudyGuide.js
@@ -0,0 +1,12 @@
+
+function StudyGuide() {
+  return (
+    <div className='main-header'>
+        <header className="App-header">
+            <h1>COMPTIA SEC+ 701 DOMAINS</h1>
+        </header>
+    </div>
+  );
+}
+
+export default StudyGuide;
